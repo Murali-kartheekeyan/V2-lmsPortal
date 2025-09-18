998ab5c16e1010b952ddc2d619ad2ef679f886a9
Murali-kartheekeyan <88695955+Murali-kartheekeyan@users.noreply.github.com>
2025-09-18 14:32:52 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,9 +4,10 @@ import os
 def get_db_connection():
     return pymysql.connect(
         host=os.getenv('DB_HOST', 'sql12.freesqldatabase.com'),
-        user=os.getenv('DB_USER', 'sql12793988'),
-        password=os.getenv('DB_PASSWORD', 'GnXSXZwr5t'),
-        database=os.getenv('DB_NAME', 'sql12793988'),  # <-- UPDATED a
+        user=os.getenv('DB_USER', 'sql12799116'),
+        password=os.getenv('DB_PASSWORD', 'spPmpX2NyE'),
+        database=os.getenv('DB_NAME', 'sql12799116'),  # <-- UPDATED a
         cursorclass=pymysql.cursors.DictCursor
 
     )   
+
